8a8ff8dd9bb0f4344e6fc239d8026fc8c8a74f96
Behruz Jumanov <behruzjumanov55@gmail.com>
2025-11-28 15:11:04 +0500

    added new changes

---
+++ script.js
@@ -0,0 +1,93 @@
+
+//1
+// let x = 45;
+
+// if(x>0) {
+// x=x+1    
+// } else {
+
+// }
+// console.log(x+ " musbat son" );
+
+//2 
+
+// let a = 10;
+
+// if (a>0) {
+//     a=a+1
+// }else  {
+//     a=a-2
+// }
+// console.log(a);
+
+//3
+
+//4 
+// let a= -12;
+// let b= 13;
+// let c = 14;
+
+// if (a>0) {
+//     console.log("a son musbat");
+// }else {
+//     console.log("a son manfiy");
+// }
+
+// if (b>0) {
+//     console.log("b son musbat");
+    
+// }else {
+//     console.log("b son manfiy");
+    
+// }
+
+// if(c>0) {
+//     console.log("c son musbat");
+    
+// }else {
+//     console.log("c son manfiy");
+    
+// }
+
+//5 
+// let x = 15;
+// let y = -18;
+// let z = -20;
+
+// if (x>0) {
+//     console.log("x soni musbat");
+    
+// }else {
+//     console.log("x soni manfiy");
+    
+// }
+
+// if(y>0) {
+//     console.log("y soni musbat");   
+// }else {
+//     console.log("y soni manfiy");
+// }
+// if (z<0) {
+//     console.log("z soni manfiy");
+    
+// }else {
+//     console.log("z soni musbat");
+    
+// }
+
+//6 
+ let a = 30;
+let v = 32;
+
+if (a>0>v) {
+    console.log("a son katta");
+    
+}else {
+    console.log("v son katta");
+    
+}
+
+//7
+ let s = 22;
+ let d = 133;
+ 
